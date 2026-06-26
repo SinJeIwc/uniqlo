6bf4dfd7d498d5d35024319e7d58785d86b55d28
Bekzhan <hp.arstanaliev@gmail.com>
2026-06-26 23:28:52 +0600

    Remove obsolete gender pages

--- frontend/src/app/baby/page.tsx
+++
@@ -1,6 +0,0 @@
-import { GenderPageContent } from "@/app/men/page";
-import homepageData from "@/data/home/baby.json";
-
-export default function BabyPage() {
-  return <GenderPageContent gender="baby" campaigns={homepageData} />;
-}
--- frontend/src/app/kids/page.tsx
+++
@@ -1,6 +0,0 @@
-import { GenderPageContent } from "@/app/men/page";
-import homepageData from "@/data/home/kids.json";
-
-export default function KidsPage() {
-  return <GenderPageContent gender="kids" campaigns={homepageData} />;
-}
