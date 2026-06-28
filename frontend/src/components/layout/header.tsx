97ce81fc5755f547dac39b7e22854747e10edc4e
Bekzhan <hp.arstanaliev@gmail.com>
2026-06-29 01:59:16 +0600

    Update header.tsx

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -13,7 +13,7 @@ export function Header({ activeTab }: HeaderProps) {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 w-full text-white bg-linear-to-b from-black/40 to-transparent text-shadow-md">
       {/* Main row */}
-      <div className="container mx-auto lg:px-4.5 xl:px-9 h-14 lg:h-16 flex items-center text-shadow-current">
+      <div className="container mx-auto px-1 lg:px-4.5 xl:px-9 h-14 lg:h-16 flex items-center text-shadow-current">
         <HeaderLogo />
         <NavbarDesktop activeTab={activeTab} />
 
